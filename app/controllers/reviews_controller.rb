97d932c78995adb822a1497b3b423b56d48b51b7
Sam Singer <samsingerjr@gmail.com>
2015-04-20 09:46:09 -0500

    finished reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,19 @@
 class ReviewsController < ApplicationController
+  before_filter :require_user
+  def review_params
+  end
+
   def create
-    video = Video.find(params[:video_id])
-    video.reviews.create(params[:review].merge!(user: current_user))
+     video = Video.find(params[:video_id])
+     video.reviews.create(review_params.merge!(user: current_user))
     redirect_to video  
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit( :rating, :content)
+    #@review = Review.new(review_params)
   end
 end
 
